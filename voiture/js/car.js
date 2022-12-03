89b465ff8753385447ebf136d3636da0b3b78db4
Syedha Memouna MOHAMED KAMALUDEEN <syedhamemouna@gmail.com>
2022-12-03 21:52:22 +0100

    réussi peut-etre

--- voiture/js/car.js
+++ voiture/js/car.js
@@ -16,13 +16,13 @@ let material1 = new THREE.MeshBasicMaterial({color : 0x00ffff});
 let cube1 = new THREE.Mesh(geometry1, material1);
 scene.add(cube1);
 
-/*let vitesse = 0;
-let max_vitesse = 3;
-let friction = 0.007;
-let acceleration = 0.2;
+let vitesse = 0;
+let max_vitesse = 0.3;
+let friction = 0.07;
+let acceleration = 0.15;
 let angle = 0;
 
-document.addEventListener('keydown', function(event) {
+/*document.addEventListener('keydown', function(event) {
     if(event.keyCode == 38) {
         vitesse += acceleration;
     }
@@ -57,33 +57,47 @@ document.addEventListener('keydown', function(event) {
 var keyboard = new THREEx.KeyboardState();
 var clock = new THREE.Clock();
 
-
 function update()
 {
-	var delta = clock.getDelta(); // seconds.
-	var moveDistance = 12 * delta; // 200 pixels per second
-	var rotateAngle = Math.PI / 2 * delta;   // pi/2 radians (90 degrees) per second
-	
-	if ( keyboard.pressed("A") )
-		cube.rotation.y += rotateAngle;
-	if ( keyboard.pressed("D") )
-		cube.rotation.y -= rotateAngle;
-			
-	if ( keyboard.pressed("left") )
+	var delta = clock.getDelta(); // seconds. // 200 pixels per second   // pi/2 radians (90 degrees) per second
+	if ( keyboard.pressed("up") ) {
+		vitesse += acceleration;}
+	if ( keyboard.pressed("down") ){
+		vitesse -= acceleration;}
+
+    if(vitesse > max_vitesse) {
+        vitesse = max_vitesse;
+    }
+    if(vitesse < -max_vitesse/2) {
+        vitesse = -max_vitesse/2
+    }
+    if(vitesse > 0) {
+        vitesse -= friction;
+    }
+    if(vitesse < 0) {
+        vitesse += friction;
+    }
+    if(Math.abs(vitesse) < friction) {
+        vitesse = 0;
+    }
+	/*if ( keyboard.pressed("left") )
 		cube.position.x -= moveDistance;
 	if ( keyboard.pressed("right") )
-		cube.position.x += moveDistance;
-	if ( keyboard.pressed("up") )
-		cube.position.z -= moveDistance;
-	if ( keyboard.pressed("down") )
-		cube.position.z += moveDistance;
+		cube.position.x += moveDistance;*/
+    if ( keyboard.pressed("right") ) {
+		angle += 0.07;}
+	if ( keyboard.pressed("left") ){
+		angle -= 0.07;}
+
+    cube.position.x -= Math.sin(angle) * vitesse;
+    cube.position.z -= Math.cos(angle) * vitesse;
 }
 
 function animate() {
     requestAnimationFrame(animate);
     /*cube.position.x - (0.5/0.2);
     cube.position.z - (0.5/0.2);*/
-    //cube.rotation.y = angle;
+    cube.rotation.y = angle;
     //camera.rotation.y = angle;
     update();
     camera.position.x = cube.position.x;
